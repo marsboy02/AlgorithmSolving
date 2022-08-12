1e8d3a01e48906c578ae38ac8ffd2eb1d1cf5179
HyeongJun Kang <86903678+Marsboy02@users.noreply.github.com>
2022-08-12 14:28:54 +0900

    [Bronze III] Title: TGN, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for _ in range(n):
+    r,e,c = map(int,input().split())
+
+    if e-c == r:
+        print('does not matter')
+    elif e-c > r:
+        print('advertise')
+    else:
+        print('do not advertise')
